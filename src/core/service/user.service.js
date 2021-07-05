f447879f176eacf8e13a94a30f4533d62ee3f23a
TanHN <tanhn.3004@gmail.com>
2021-07-05 12:31:40 +0700

    Movie management - Pagination, Add, Delete

---
+++ src/core/service/user.service.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { DOMAIN } from "../global/constant";
+
+// temp
+export const TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoidGFuaG4iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJRdWFuVHJpIiwibmJmIjoxNjI1MDM2NTI5LCJleHAiOjE2MjUwNDAxMjl9.3-NV6J-FhB0p2bhAjFOvBCQKIdBdc3EONir5RXq-7Ug";
+
+class UserService {
+  getUserListPagination = (groupID, pageNumber, itemPerPageNumber) =>
+    axios({
+      url: `${DOMAIN}/api/QuanLyNguoiDung/LayDanhSachNguoiDungPhanTrang?MaNhom=${groupID}&soTrang=${pageNumber}&soPhanTuTrenTrang=${itemPerPageNumber}`,
+      method: "GET"
+    });
+
+  addUser = (data) =>
+    axios({
+      url: `${DOMAIN}/api/QuanLyNguoiDung/ThemNguoiDung`,
+      method: "POST",
+      data,
+      headers: {
+        Authorization: `Bearer ${TOKEN}`
+      }
+    });
+
+  deleteUser = (taiKhoan) =>
+    axios({
+      url: `${DOMAIN}/api/QuanLyNguoiDung/XoaNguoiDung?TaiKhoan=${taiKhoan}`,
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${TOKEN}`
+      }
+    });
+
+  updateUser = (data) =>
+    axios({
+      url: `${DOMAIN}/api/QuanLyNguoiDung/CapNhatThongTinNguoiDung`,
+      method: "PUT",
+      data,
+      headers: {
+        Authorization: `Bearer ${TOKEN}`
+      }
+    });
+
+  searchUserPagination = (
+    searchString,
+    groupID,
+    pageNumber,
+    itemPerPageNumber
+  ) =>
+    axios({
+      url: `${DOMAIN}/api/QuanLyNguoiDung/TimKiemNguoiDungPhanTrang?MaNhom=${groupID}&tuKhoa=${searchString}&soTrang=${pageNumber}&soPhanTuTrenTrang=${itemPerPageNumber}`,
+      method: "GET"
+    });
+}
+
+export const userService = new UserService();
