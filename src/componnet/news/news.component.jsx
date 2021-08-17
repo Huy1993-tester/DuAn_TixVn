50d189d75928a5d0d843a4f3bd7028cfa25e5ca1
TanHN <tanhn.3004@gmail.com>
2021-08-17 22:41:21 +0700

    Add animation for newsBlock

--- src/componnet/news/news.component.jsx
+++ src/componnet/news/news.component.jsx
@@ -4,36 +4,19 @@ import comment from "../../asset/image/comment.png";
 import cinemaList from "./cinema.data.json";
 import reviewList from "./review.data.json";
 import promotionList from "./promotion.data.json";
+import LazyLoad from "react-lazyload";
 
 import React, { useState } from "react";
+import clsx from "clsx";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
 
-const News = () => {
-  const [value, setValue] = useState("1");
-
-  const handleChange = (e) => {
-    const headerTab = document.querySelectorAll(".header__tab");
-    headerTab.forEach((tab) => {
-      tab.className = `${style.header__tab} header__tab`;
-    });
-    e.currentTarget.className += " active";
-    setValue(e.target.value);
-  };
-
-  const renderNews = () => {
-    let array = [];
-    switch (value) {
-      case "1":
-        array = [...cinemaList];
-        break;
-      case "2":
-        array = [...reviewList];
-        break;
-      case "3":
-        array = [...promotionList];
-        break;
-    }
-    const news = array.slice(0, 4).map((d, index) => {
-      return (
+const NewsChild = React.memo(({ array }) => {
+  console.log("render");
+  const news = array.slice(0, 4).map((d, index) => {
+    return (
+      <>
         <div
           key={index}
           className={
@@ -64,40 +47,60 @@ const News = () => {
             </span>
           </div>
         </div>
-      );
-    });
-    const smallNews = array.slice(4, 8).map((d, index) => {
-      return (
-        <div
-          key={index}
-          className={`row m-0 mb-3 ${style.news__item} ${style.news__item__small}`}
-        >
-          <div className="col-2 col-sm-3 col-lg-2 p-0">
-            <div className={style.news__image}>
-              <a target="_blank" href="#">
-                <img src={d.image2} />
-              </a>
-            </div>
-          </div>
-          <h4 className={`${style.news__title} col-10 col-sm-9 col-lg-10`}>
-            <a target="_blank" href="#">
-              {d.title}
-            </a>
-          </h4>
-        </div>
-      );
-    });
-    return (
-      <>
-        {news}
-        <div className={`col-sm-4 ${style.news__small}`}>{smallNews}</div>
       </>
     );
+  });
+  const smallNews = array.slice(4, 8).map((d, index) => {
+    return (
+      <div
+        key={index}
+        className={`row m-0 mb-3 ${style.news__item} ${style.news__item__small}`}
+      >
+        <div className="col-2 col-sm-3 col-lg-2 p-0">
+          <div className={style.news__image}>
+            <a target="_blank" href="#">
+              <img src={d.image2} />
+            </a>
+          </div>
+        </div>
+        <h4 className={`${style.news__title} col-10 col-sm-9 col-lg-10`}>
+          <a target="_blank" href="#">
+            {d.title}
+          </a>
+        </h4>
+      </div>
+    );
+  });
+  return (
+    <>
+      {news}
+      <div className={`col-sm-4 ${style.news__small}`}>{smallNews}</div>
+    </>
+  );
+});
+
+const News = () => {
+  const [value, setValue] = useState("1");
+
+  const handleChange = (e) => {
+    const headerTab = document.querySelectorAll(".header__tab");
+    headerTab.forEach((tab) => {
+      tab.className = `${style.header__tab} header__tab`;
+    });
+    e.currentTarget.className += " active";
+    setValue(e.target.value);
   };
 
   return (
     <section id="newsBlock" className={style.new__section}>
-      <div className={style.wrapper}>
+      <div
+        data-aos="fade-up"
+        data-aos-delay="50"
+        data-aos-duration="600"
+        data-aos-easing="ease-in-out"
+        data-aos-anchor-placement="top-center"
+        className={style.wrapper}
+      >
         <div className={style.news__content}>
           <div className={`${style.news__header}`}>
             <button
@@ -123,10 +126,32 @@ const News = () => {
             </button>
           </div>
           <div className={style.news__body}>
-            <div className="">
-              <div className={`row m-0 ${style.news__main}`}>
-                {renderNews()}
-              </div>
+            <div
+              className={clsx({
+                "row m-0": true,
+                [style.news__main]: true,
+                active: value === "1"
+              })}
+            >
+              <NewsChild array={cinemaList} />
+            </div>
+            <div
+              className={clsx({
+                "row m-0": true,
+                [style.news__main]: true,
+                active: value === "2"
+              })}
+            >
+              <NewsChild array={reviewList} />
+            </div>
+            <div
+              className={clsx({
+                "row m-0": true,
+                [style.news__main]: true,
+                active: value === "3"
+              })}
+            >
+              <NewsChild array={promotionList} />
             </div>
           </div>
         </div>
